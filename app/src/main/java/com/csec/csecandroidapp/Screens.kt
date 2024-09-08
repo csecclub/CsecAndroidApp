42640a90772b1ea949ab6f161502f4cb4bed72ca
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-09-07 19:25:36 -0500

    FEAT: Added base club project template. TODO: Update project from MOBI info to CSEC info and change the style a lot

---
+++ app/src/main/java/com/csec/csecandroidapp/Screens.kt
@@ -0,0 +1,11 @@
+package com.csec.csecandr           oidapp
+
+sealed class Screens(val screen: String) {
+
+    // main app data objects
+    data object Home : Screens("home")
+    data object About : Screens("about")
+
+    data object Calendar : Screens("calendar")
+    data object Officers : Screens("officers")
+}
